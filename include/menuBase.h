35e88d71f986b47ca98551e5269de6430ea95973
dimmkin <120401947+dimmkin@users.noreply.github.com>
2023-11-26 18:39:27 +0300

    Settigns -> Settings

--- include/menuBase.h
+++ include/menuBase.h
@@ -81,11 +81,11 @@ namespace game {
 		Exit(sf::RenderWindow& window, double menux, double menuy,
 			int index, sf::String name[], int sizeFont = 60, int step = 80);
 	};
-	# Settigs надо исправить на Settings
-	class Settigns : public MenuBase
+	
+	class Settings : public MenuBase
 	{
 	public:
-		Settigns(sf::RenderWindow& window, double settignsmenux, double settignsy,
+		Settings(sf::RenderWindow& window, double settingsmenux, double settingsy,
 			int index, sf::String name[], int sizeFont = 60, int step = 80);
 	};
 }
