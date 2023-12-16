170a27818a63e816d76dd94f336f571093368293
dimkin <22350307@moscow.eu>
2023-12-16 20:58:27 +0300

    added directory docs/

---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "ДП",
+  1: "ДП"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Pages"
+};
+
